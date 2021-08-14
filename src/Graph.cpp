2b0dc835cef7d4a47953c08d8fc8295284f2a4fc
Bpangeni <64627635+Bpangeni@users.noreply.github.com>
2021-08-14 09:44:35 +0545

    random graph update
    By bishwa

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -186,6 +186,53 @@ Graph::Graph(bool directed) : directed(directed) {
         adjacencyMatrix[i][j] = 0;
 }
 
+void Graph::GenerateRandomGraphs(const int NOE, int NOV)
+{
+	int i, j, edge[NOE][2], count;
+	i = 0;
+	// connecting two vertex.
+	while(i < NOE)
+	{
+		edge[i][0] = rand()%NOV+1;
+		edge[i][1] = rand()%NOV+1;
+ 
+		if(edge[i][0] == edge[i][1])
+			continue;
+		else
+		{
+			for(j = 0; j < i; j++)
+			{
+				if((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) || (edge[i][0] == edge[j][1] && edge[i][1] == edge[j][0]))
+					i--;
+			}
+		}
+		i++;
+	}
+ 
+	std::cout<<"The generated random random graph is:\n ";
+	for(i = 0; i < NOV; i++)
+	{
+		count = 0;
+		std::cout<<"\n\t"<<i+1<<"-> { ";
+		for(j = 0; j < NOE; j++)
+		{
+			if(edge[j][0] == i+1)
+			{
+				std::cout<<edge[j][1]<<" ";
+				count++;
+			}
+			else if(edge[j][1] == i+1)
+			{
+				std::cout<<edge[j][0]<<" ";
+				count++;
+			}
+			else if(j == NOE-1 && count == 0)
+				std::cout<<"Isolated Vertex!";
+		}
+		std::cout<<"}";
+	}	
+}
+
 
 
 
